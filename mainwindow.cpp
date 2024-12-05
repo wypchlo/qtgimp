41e9665c6753a85b35f1f633b0d6b09a3f37d646
wypchlo <deron.pl.yt@gmail.com>
2024-12-05 12:12:20 +0100

    Created the main app widget added a new widget and set it as the main app widget

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,11 @@
+#include <QtWidgets>
+
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
+
     setWindowTitle("QTGimp - " + tr("Main window"));
     setMinimumSize(300, 200);
     resize(600, 400);
