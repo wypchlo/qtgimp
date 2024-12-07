8aa43b08f90c3b5b2348d36c8d6501e35dd34b97
wypchlo <deron.pl.yt@gmail.com>
2024-12-07 13:49:51 +0100

    Added support for PBM color invertion

--- imagemodifier.cpp
+++ imagemodifier.cpp
@@ -11,11 +11,19 @@ void ImageModifier::invertColor(QImage *image) {
     int width = image->width();
     int height = image->height();
 
-    if(image->format() == QImage::Format_RGB888) {
+    if(imageFormat == QImage::Format_RGB888) {
         for( int y = 0; y < height; y++ ) {
             for( int x = 0; x < width; x++ ) {
                 QColor color = image->pixelColor(x, y);
                 image->setPixelColor(x, y, qRgb(255 - color.red(), 255 - color.green(), 255 - color.blue()));
+            }
+        }
+    }
+    else if(imageFormat == QImage::Format_Mono) {
+        for( int y = 0; y < height; y++ ) {
+            for( int x = 0; x < width; x++ ) {
+                bool color = image->pixelColor(x, y).red();
+                image->setPixel(x, y, !color);
             }
         }
     }
